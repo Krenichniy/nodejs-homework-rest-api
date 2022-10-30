24c3477f1569aa5c02cf5049c0ca219629730308
Yevhen <73534149+Krenichniy@users.noreply.github.com>
2022-10-30 10:48:06 +0300

    added users avatar adding

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { validateBody, authenticate } = require('../../middlewares');
+const { validateBody, authenticate, upload } = require('../../middlewares');
 const {ctrlWrapper} = require('../../helpers');
 const { schemas } = require('../../models/user');
 const ctrl = require('../../controllers/auth');
@@ -9,5 +9,7 @@ router.post('/register', validateBody(schemas.registerShema), ctrlWrapper(ctrl.r
 router.post('/login', validateBody(schemas.loginShema), ctrlWrapper(ctrl.login) );
 router.get('/current', authenticate, ctrlWrapper(ctrl.getCurrent));
 router.get('/logout', authenticate, ctrlWrapper(ctrl.logout));
+// router.patch('/users/subscription', authenticatem, ctrlWrapper(ctrl.updateSubscription));
+router.patch('/users/avatars', authenticate, upload.single("avatar"), ctrlWrapper(ctrl.updateAvatar));
 
 module.exports = router;
